97437993d4a775a78dbfbf15835501e3d3f94f0c
George Jor <george@9gag.com>
2017-01-16 10:13:48 +0800

    #1 Commit built folder for development

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+Object.defineProperty(exports,"__esModule",{value:true});var _SnackBarManager=require('./SnackBarManager');var _SnackBarManager2=_interopRequireDefault(_SnackBarManager);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}exports.default=
+
+new _SnackBarManager2.default();
+//# sourceMappingURL=index.js.map
